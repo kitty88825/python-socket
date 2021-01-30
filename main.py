d402d1f05ba4e5f17f7cc97b083ff8d90ce07ce8
kitty88825 <kitty40632@gmail.com>
2021-01-31 01:44:44 +0800

    Remove encoding='utf-8' to the encode function

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ while True:
     msg = "<html>Hello World</html>"
     response_headers = {
         'Content-Type': 'text/html; encoding=utf8',
-        'Content-Length': len(msg.encode(encoding="utf-8")),
+        'Content-Length': len(msg.encode()),
         'Connection': 'close',
     }
     response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.items())  # noqa: E501
 
-    client.send(response_status.encode(encoding="utf-8"))
-    client.send(response_headers_raw.encode(encoding="utf-8"))
-    client.send('\n'.encode(encoding="utf-8"))
-    client.send(msg.encode(encoding="utf-8"))
+    client.send(response_status.encode())
+    client.send(response_headers_raw.encode())
+    client.send('\n'.encode())
+    client.send(msg.encode())
     client.close()
 
 server.close()
